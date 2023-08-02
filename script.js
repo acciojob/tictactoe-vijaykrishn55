9bc87952ed6472aa4f382aed3ed561bceec348d2
vijaykrishn55 <119862236+vijaykrishn55@users.noreply.github.com>
2023-08-02 13:22:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+const button=document.querySelectorAll('div>button');
+var player1,player2;
+button[0].addEventListener("click",verifyfc);
+function verifyfc{
+	
+}
